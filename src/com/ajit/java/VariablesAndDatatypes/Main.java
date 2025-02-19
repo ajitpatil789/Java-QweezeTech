7aeb10b83ba75a9f25118376e0e7bf5855fdd2c1
Ajit Patil <ajitpatil789@gmail.com>
2025-02-19 09:36:10 +0530

    Variables and Data types commit

---
+++ src/com/ajit/java/VariablesAndDatatypes/Main.java
@@ -0,0 +1,32 @@
+package com.ajit.java.VariablesAndDatatypes;
+
+// Variables and Data types.
+
+public class Main {
+    public int ten = 10;
+
+    public static void main(String[] args) {
+        // Declare and initialize variables of different data types
+        byte byteValue = 12;
+        short shortValue = 123;
+        int intValue = 100;
+        float floatValue = 10.5f;
+        double doubleValue = 10.52;
+        long longValue = 537810106698L;
+        char charValue = 'a';
+        boolean isAdult = true;
+        String stringValue = "Welcome to Java World";
+
+        // Print the values of the variables
+        System.out.println("Byte: " + byteValue);
+        System.out.println("Short: " + shortValue);
+        System.out.println("Int: " + intValue);
+        System.out.println("Float: " + floatValue);
+        System.out.println("Double: " + doubleValue);
+        System.out.println("Long: " + longValue);
+        System.out.println("Char: " + charValue);
+        System.out.println("String: " + stringValue);
+        System.out.println("Boolean: " + isAdult);
+
+    }
+}
